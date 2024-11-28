2d0156d63554f9b30537ab1206bc6371c5501e14
khubaib shah <bewithbaacha@gmail.com>
2024-11-28 17:04:30 +0500

    Card ui and elements funtionality done

--- js/element.js
+++ js/element.js
@@ -2,10 +2,35 @@ class Elements {
   constructor(cardsNumber) {
     this.cardsNumber = cardsNumber;
     this.playground = document.querySelector(".playground");
+    this.cardsIndex = [];
+    this.doubleindex();
     this.createCard();
   }
+  doubleindex() {
+    for (var i = 1; i <= this.cardsNumber; i++) {
+      i <= this.cardsNumber / 2
+        ? this.cardsIndex.push(i)
+        : this.cardsIndex.push(i - this.cardsNumber / 2);
+    }
+    console.log(this.cardsIndex);
+  }
   createCard() {
-    console.log(this.cardsNumber);
+    this.playground.style.gridTemplateRows = `repeat(${Math.sqrt(
+      this.cardsNumber
+    )}, 1fr)`;
+    this.playground.style.gridTemplateColumns = `repeat(${Math.sqrt(
+      this.cardsNumber
+    )}, 1fr)`;
+    this.cardsIndex.forEach((index) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.setAttribute("data-index", index);
+
+      const img = document.createElement("img");
+      img.src = `images/icon-${index}.png`;
+      card.append(img);
+      this.playground.append(card);
+    });
   }
 }
 export default Elements;
